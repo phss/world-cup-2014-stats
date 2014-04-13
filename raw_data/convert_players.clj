83f33cf18d9df3549e8e971287892392e988476d
Paulo Schneider <paulo.schneider@gmail.com>
2014-04-13 16:20:52 +0100

    Basic player conversion

--- raw_data/convert_players.clj
+++ raw_data/convert_players.clj
@@ -6,7 +6,19 @@
         split-by-tab #(split % #"\t")]
     (map split-by-tab lines)))
 
+(defn convert-player [country data]
+  (let []
+    {
+      :id (str country "_" (data 0))
+      :country country
+      :name (data 1)
+      :shirt_number (data 0)
+      :date_of_birth (data 2)
+      :position (data 3)
+      :club (data 4)
+      :height (data 5)
+    }))
 
 (def raw (read-tabular-file "2010/brazil.data"))
 
-(println (ffirst raw))
+(println (convert-player "brazil" (first raw)))
