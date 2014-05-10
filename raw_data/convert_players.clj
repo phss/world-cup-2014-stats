46c0043f1c497f32f7122259cf50fe68414b3391
Paulo Schneider <paulo.schneider@gmail.com>
2014-05-10 12:33:23 +0100

    Convert by country name

--- raw_data/convert_players.clj
+++ raw_data/convert_players.clj
@@ -5,7 +5,7 @@
 (def position { "GK" "Goalkeeper", "DF" "Defender", "MF" "Midfielder", "FW" "Forward" })
 
 (def country-by-code {
-  "BRA" "Brazil", "ENG" "England", "ESP" "Spain", "FRA" "France", "GER" "Germany", "GRE" "Greece", "ITA" "Italy", "POR" "Portugal", "TUR" "Turkey"                      
+  "ALG" "Algeria", "BRA" "Brazil", "BUL" "Bulgaria", "ENG" "England", "ESP" "Spain", "FRA" "France", "GER" "Germany", "GRE" "Greece", "ITA" "Italy", "POR" "Portugal", "SCO" "Scotland", "TUR" "Turkey"                      
 })
 
 (def number read-string)
@@ -26,6 +26,13 @@
       (throw (Exception. (str "No country for " country-code)))
       { :name club, :country country })))
 
+; Reading from 
+(defn read-tabular-file [filename]
+  (let [data (slurp filename)
+        lines (split-lines data)
+        split-by-tab #(split % #"\t")]
+    (map split-by-tab lines)))
+
 ; Player conversion
 (defn convert-player [country data]
   (let [shirt (number (data 0))]
@@ -40,17 +47,22 @@
       :height (number (data 5))
     }))
 
-; Reading from 
-(defn read-tabular-file [filename]
-  (let [data (slurp filename)
-        lines (split-lines data)
-        split-by-tab #(split % #"\t")]
-    (map split-by-tab lines)))
+(defn convert-country [file]
+  (let [filename (. file getName)
+        country (second (re-find #"(\w+)." filename)) 
+        raw (read-tabular-file file)]
+    (map #(convert-player country %) raw)))
 
 
 (def raw (read-tabular-file "2010/brazil.data"))
 
 (def brazil (map #(convert-player "brazil" %) raw))
 
-(println (json/write-str brazil))
+;(println (json/write-str brazil))
 
+(def directory (clojure.java.io/file "2010/"))
+(def files (rest (file-seq directory)))
+(def file (first files))
+(def filename (. file getName))
+
+(println (convert-country file))
